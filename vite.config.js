3e55b13462dac638570ad8ec49f931d7327dab1e
olehkl1apko <kliapko.oleh.dev@gmail.com>
2024-03-27 17:10:56 +0200

    ADD: million lint

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
+import MillionLint from '@million/lint';
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+var plugins = [react()];
+plugins.unshift(MillionLint.vite())
 export default defineConfig({
-  plugins: [react()],
-
+  plugins: plugins,
   server: {
-    port: 3000,
-  },
+    port: 3000
+  }
 });
